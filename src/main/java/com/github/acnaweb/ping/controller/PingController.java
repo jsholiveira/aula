1b7c949fff5643c54607d47aa06280de9b2ed259
AC <ac@marketmining.com.br>
2023-08-10 19:46:56 -0300

    init

--- src/main/java/com/github/acnaweb/ping/controller/PingController.java
+++ src/main/java/com/github/acnaweb/ping/controller/PingController.java
@@ -12,6 +12,6 @@ public class PingController {
 
     @GetMapping("/ping")
     public String ping() {
-        return "pong " + Calendar.getInstance().toString();
+        return "pong ";
     }
 }
