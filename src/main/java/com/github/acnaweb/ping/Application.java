5bc9b58b04df9953771478234bd7247314472398
AC <ac@marketmining.com.br>
2023-08-17 20:56:14 -0300

    fix

--- src/main/java/com/github/acnaweb/ping/Application.java
+++ src/main/java/com/github/acnaweb/ping/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PingApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(PingApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
